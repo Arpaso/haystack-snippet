46fea23d13892782a99f56fc64f8e32867089fcb
Arvid Paeglit <arvid@arpaso.com>
2012-01-05 13:52:17 +0400

    debug queryset

--- src/haystack_snippet/query.py
+++ src/haystack_snippet/query.py
@@ -11,7 +11,7 @@ class RelatedSearchQuerySet(RelatedSearchQuerySetOld):
     def _get_count(self):
         """Returns the total number of matching results."""
         results = self.query.get_results()
-        raise TypeError(len(list(results)))
+        raise AttributeError(len(results))
         # Remember the search position for each result so we don't have to resort later.
         models_pks = {}
         for result in results:
