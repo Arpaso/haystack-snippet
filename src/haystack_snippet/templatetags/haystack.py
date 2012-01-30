8a9776a5cb6c2ebdd1b8870504544d0c8329b9d3
predatell <seryi_belyi@inbox.ru>
2012-01-30 18:35:50 +0200

    add prefix

--- src/haystack_snippet/templatetags/haystack.py
+++ src/haystack_snippet/templatetags/haystack.py
@@ -6,5 +6,6 @@ from haystack_snippet.forms import HaystackSearchForm
 
 @register.inclusion_tag('search/search_form.html')
 def show_search_form(request):
-    form = HaystackSearchForm(request.GET or None)
+    form = HaystackSearchForm(request.GET or None, prefix="search")
+    print form
     return { "search_form": form }
