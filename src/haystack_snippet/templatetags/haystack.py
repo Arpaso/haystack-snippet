bef6eb0367d1e3fc177dae83b8365c8b013bc0bf
predatell <seryi_belyi@inbox.ru>
2012-01-30 16:44:42 +0200

    builtin search form

--- src/haystack_snippet/templatetags/haystack.py
+++ src/haystack_snippet/templatetags/haystack.py
@@ -2,6 +2,9 @@ from django import template
 
 register = template.Library()
 
+from haystack_snippet.forms import HaystackSearchForm
+
 @register.inclusion_tag('search/search_form.html')
-def show_seach_form():
-    return { }
+def show_search_form(request):
+    form = HaystackSearchForm(request.GET or None)
+    return { "search_form": form }
